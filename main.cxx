6fd71527842dc8c1405b0dff610796aa8dee122c
Sascha Brunner <sascha.brunner@outlook.com>
2020-04-01 14:41:31 +0200

    Add vertex buffer and basic shaders

--- main.cxx
+++ main.cxx
@@ -3,6 +3,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+void checkShaderCompileSuccess(GLuint shader);
 void framebufferSizeCallback(GLFWwindow *window, int widht, int height);
 void processInput(GLFWwindow *window);
 
@@ -34,6 +35,68 @@ int main()
     // state setting, call once
     glClearColor(.8f, .3f, .3f, 1.0f);
 
+    // vertices for a triangle
+    GLfloat vertices[] = {
+        -0.5f,
+        -0.5f,
+        0.0f,
+        0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        0.5f,
+        0.0f};
+
+    // vertex buffer object
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // tell OpenGL how to interpret vertex data and enable it as the
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // vertex shader
+    const char *vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 pos;\n"
+                                     "\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "    gl_Position = vec4(pos, 1.0);\n"
+                                     "}\0";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompileSuccess(vertexShader);
+
+    // fragment shader
+    const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 color;\n"
+                                       "\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\0";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompileSuccess(fragmentShader);
+
+    // create shader program and link shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+
+    // delete shaders that have been linked to a program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     while (!glfwWindowShouldClose(window))
     {
         // input
@@ -50,6 +113,34 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+void checkProgramCompileSuccess(GLuint program)
+{
+    GLint success;
+    glGetProgramiv(program, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        char infoLog[512];
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cout << "Shader compilation failed\n"
+                  << infoLog << std::endl;
+    }
+}
+
+void checkShaderCompileSuccess(GLuint shader)
+{
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "Shader compilation failed\n"
+                  << infoLog << std::endl;
+    }
 }
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height)
